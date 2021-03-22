2bdb8533a061e252d92b57ced6413e11bfface56
Frida YAN <hyan6@jaguarlandrover.com>
2021-03-22 15:31:30 +0800

    Jms config

---
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -0,0 +1,18 @@
+package guru.sfg.beer.inventory.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+    @Bean //Serialize message content to json using textmessage
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
