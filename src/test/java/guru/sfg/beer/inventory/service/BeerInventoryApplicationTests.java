487c2afbd6eaf7f5ce44ec0d961ca46480845871
Frida YAN <hyan6@jaguarlandrover.com>
2021-04-28 10:02:26 +0800

    docker

--- src/test/java/guru/sfg/beer/inventory/service/BeerInventoryApplicationTests.java
+++ src/test/java/guru/sfg/beer/inventory/service/BeerInventoryApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class BeerInventoryApplicationTests {
 
-    @Test
+   @Test
     public void contextLoads() {
     }
 
